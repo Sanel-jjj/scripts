6b26d039a194c1531d007c460226b7a429e01e19
Sanel-jjj <68742311+Sanel-jjj@users.noreply.github.com>
2020-07-25 00:06:19 +0300

    Add files via upload

---
+++ new.lua
@@ -0,0 +1,57 @@
+script_name("New")
+script_authors("jjj")
+
+require "lib.moonloader"
+local key = require 'vkeys'
+local imgui = require 'imgui'
+local inicfg = require 'inicfg'
+local encoding = require 'encoding'
+local sampev = require 'lib.samp.events'
+
+encoding.default = 'CP1251'
+u8 = encoding.UTF8
+
+-- Цвета
+-- Красный {B22222}
+-- Желтый {FFFF00}
+-- Зеленый {006400}
+-- Синий {4682B4}
+
+local tag = "[X]"
+
+function main()
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+
+    sampAddChatMessage("{4682B4}" .. tag .. " {FFFFFF}Скрипт успешно запущен.",-1)
+    sampAddChatMessage("{4682B4}" .. tag .. " {FFFFFF}Версия: 0.1",-1)
+
+    sampRegisterChatCommand("xmenu", cmd_xmenu)
+
+    thread = lua_thread.create_suspended(thread_func)
+    -- Блок после включения сампа
+    while true do
+        wait(0)
+
+        local result, button, list, input = sampHasDialogRespond(1)
+        if result then
+            if button == 1 then
+                thread:run("2")
+            else return end
+        end
+        -- Бесконечный блок
+    end
+end
+
+function cmd_xmenu(arg)
+    thread:run("1")
+end
+
+function thread_func(option)
+    if option == "1" then
+        sampShowDialog(1,"1","Первый диалог","Окей","",0)
+    end
+    if option == "2" then
+        sampAddChatMessage("Все окей", -1)
+    end
+end
