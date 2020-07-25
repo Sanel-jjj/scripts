3b8669473052d5606e36940b7560558090d7c776
Sanel-jjj <68742311+Sanel-jjj@users.noreply.github.com>
2020-07-25 20:24:17 +0300

    Add files via upload

--- x.lua
+++ x.lua
@@ -11,11 +11,11 @@ local sampev = require 'lib.samp.events'
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
 
--- Цвета
--- Красный {B22222}
--- Желтый {FFFF00}
--- Зеленый {006400}
--- Синий {4682B4}
+-- пїЅпїЅпїЅпїЅпїЅ
+-- пїЅпїЅпїЅпїЅпїЅпїЅпїЅ {B22222}
+-- пїЅпїЅпїЅпїЅпїЅпїЅ {FFFF00}
+-- пїЅпїЅпїЅпїЅпїЅпїЅпїЅ {006400}
+-- пїЅпїЅпїЅпїЅпїЅ {4682B4}
 
 local tag = "[X]"
 
@@ -23,35 +23,35 @@ function main()
     if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(100) end
 
-    sampAddChatMessage("{4682B4}" .. tag .. " {FFFFFF}Скрипт успешно запущен.",-1)
-    sampAddChatMessage("{4682B4}" .. tag .. " {FFFFFF}Версия: 0.1",-1)
+    sampAddChatMessage("{4682B4}" .. tag .. " {FFFFFF}пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.",-1)
+    sampAddChatMessage("{4682B4}" .. tag .. " {FFFFFF}пїЅпїЅпїЅпїЅпїЅпїЅ: 0.1",-1)
 
     sampRegisterChatCommand("xmenu", cmd_xmenu)
 
     thread = lua_thread.create_suspended(thread_func)
-    -- Блок после включения сампа
+    -- пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
     while true do
         wait(0)
         
         if isKeyDown(VK_7) then
-            sampShowDialog(10,"{4682B4}X {FFFFFF}| Главное меню","{4682B4}1 {FFFFFF}Журнал обновлений\n{4682B4}2 {FFFFFF}Настройки \n{4682B4}3 {FFFFFF}Контакты","Выбор","",2)
+            sampShowDialog(10,"{4682B4}X {FFFFFF}| пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ","{4682B4}1 {FFFFFF}пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n{4682B4}2 {FFFFFF}пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ \n{4682B4}3 {FFFFFF}пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ","пїЅпїЅпїЅпїЅпїЅ","",2)
         end
 
         local result, button, list, imput = sampHasDialogRespond(10)
         if result then
             if button == 1 then
                 if list == 0 then
-                    sampShowDialog(11,"{4682B4}X {FFFFFF}| Журнал обновлений","24.07.2020\nxx.xx.xxxx\nxx.xx.xxxx","Выбор","Назад",2)
+                    sampShowDialog(11,"{4682B4}X {FFFFFF}| пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ","24.07.2020\nxx.xx.xxxx\nxx.xx.xxxx","пїЅпїЅпїЅпїЅпїЅ","пїЅпїЅпїЅпїЅпїЅ",2)
                 elseif list == 1 then
-                    sampShowDialog(12,"{4682B4}X {FFFFFF}| Настройки","-\n-\n-\n-","Выбор","Назад",2)
+                    sampShowDialog(12,"{4682B4}X {FFFFFF}| пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ","-\n-\n-\n-","пїЅпїЅпїЅпїЅпїЅ","пїЅпїЅпїЅпїЅпїЅ",2)
                 elseif list == 2 then
-                    sampShowDialog(13,"{4682B4}X {FFFFFF}| Контакты","{4682B4}Автор скрипта{FFFFFF}: Faust Van Helsing (jjj)\n{4682B4}VK{FFFFFF}: https://vk.com/canelhan","Назад","",0) 
+                    sampShowDialog(13,"{4682B4}X {FFFFFF}| пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ","{4682B4}пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ{FFFFFF}: Faust Van Helsing (jjj)\n{4682B4}VK{FFFFFF}: https://vk.com/canelhan","пїЅпїЅпїЅпїЅпїЅ","",0) 
                 end    
             end
         end
-    -- Бесконечный блок
+    -- пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
     end
 end
 function cmd_xmenu(arg)
-    sampAddChatMessage("Окей",-1)
+    sampAddChatMessage("пїЅпїЅпїЅпїЅ",-1)
 end
